141ce532f7885ccf21e806b60ca78e9581db7916
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-20 20:46:41 +0530

    Creating frame for the game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class TicTacToe extends JFrame {
+public class TicTacToe extends JFrame implements ActionListener {
+
+    JFrame frame = new JFrame();
+
         public TicTacToe(){
-            setTitle("Super Tic-Tac-Toe");
-            setSize(600,600);
-
+            frame.setTitle("Super Tic-Tac-Toe");
+            frame.setSize(640,640);
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setVisible(true);
         }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
