3c2ea0d6438c0e0b507eb23c5655605c08eed53e
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-20 20:31:57 +0530

    Defining TicTacToe class

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,11 @@
-public class TicTacToe {
+import javax.swing.*;
+import java.awt.*;
 
+
+public class TicTacToe extends JFrame {
+        public TicTacToe(){
+            setTitle("Super Tic-Tac-Toe");
+            setSize(600,600);
+
+        }
 }
